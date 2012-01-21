4c48f4a080d2e2fdfb6f72acf600fda72164f83d
Preston Ciere <preston@portageur.ca>
2012-01-20 23:12:24 -0500

    initial commit

---
+++ app/controllers/welcom_controller.rb
@@ -0,0 +1,5 @@
+class WelcomController < ApplicationController
+  def index
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = '1185c8667bce9814242e7e6143b95de89b92f1c59c928e03ba168845be6fcfe2d9211c966cd599a8e38ffbe0baa50a02b945bbc2f2ea43f366f3f98cc6dcd706'
